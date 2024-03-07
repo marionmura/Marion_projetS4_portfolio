2129f4b1d2dfc65ec50989f3f095805053c4de47
steak-osaure <marion.mura090@gmail.com>
2024-03-07 08:54:51 +0100

    formulaire responsive

--- wp-content/themes/steaktheme/contact.php
+++ wp-content/themes/steaktheme/contact.php
@@ -5,8 +5,8 @@ Template Name: Contact
 
 get_header();
 ?>
-<div class="bg-blanc-ivoir grid grid-rows-[1fr] ">
-    <div class="items-center gap-0 py-10">
+<div class="bg-blanc-ivoir grid grid-cols-1  grid-center justify-items-center ">
+    <div class="gap-0 py-10">
         <div class="z-0">
             <img class="absolute w-[480px] left-[-100px] top-[50px] md:top-[0px] sm:w-[400px] sm:top-[-110px] sm:left-[-40px] "
                 src="<?php
@@ -20,17 +20,17 @@ get_header();
         </div>
 
     </div>
-    <div class="z-40 pt-[50px] mx-[15rem] pb-[8rem]">
-        <p class="text-gris text-[4rem] font-Paytone md:text-2xl sm:text-[2rem] ">Besoin de me contacter ?</p>
+    <div class="items-center z-40 pt-[50px] mx-[15rem] pb-[8rem]">
+        <h1 class="text-gris text-[4rem] font-Paytone md:text-2xl sm:text-[2rem]">Besoin de me contacter?</h1>
+        <p class="mt-4 text-[24px] md:text-[20px] sm:text-[16px]">Besoin d'un renseignement ou envie de me faire part de vos idées ? N'hésitez pas à remplir ce formulaire, je suis à votre écoute !</p>
+            </p>
     </div>
 
 
-    <div class='sm:mx-[4vw] md:mx-[10vw] mx-[20vw] mt-10'>
-
+    <div class='z-40 sm:mx-[2vw] md:mx-[8vw] mx-[20vw] mt-10'>
         <div class='md:px-3 lg:px-14 px-32 rounded-lg  text-[25px]'>
             <form class="" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
-
-                <div class="font-Paytone my-4 bg-blanc-ivoir">
+                <div class="font-Paytone my-4">
                     <div class="grid grid-rows-2">
                         <!-- <label for="email">Email</label> -->
                         <input type="email" id="mail" placeholder="Email" name="mail" required>
@@ -51,23 +51,25 @@ get_header();
 
                     <div class="grid grid-rows-2 pt-20 ">
                         <label class="font-Paytone mb-2" for="object">Sujet du message</label>
-                        <input class="text-[18px] bg-blanc-ivoir" name="object" placeholder="Ici l'objet de votre message">
+                        <input class="text-[18px] bg-blanc-ivoir" name="object"
+                            placeholder="Ici l'objet de votre message">
                         <span class="w-full h-[3px] bg-gris"></span>
                     </div>
                     <div class="grid grid-rows-2 pt-20 pb-20">
                         <label class="" for="content">Message</label>
-                        <input name="content" id="content" class="text-[18px] bg-blanc-ivoir" placeholder="Pourquoi souhaitez-vous me contacter ?"></input>
+                        <input name="content" id="content" class="text-[18px] bg-blanc-ivoir"
+                            placeholder="Pourquoi souhaitez-vous me contacter ?"></input>
                         <span class="w-full h-[3px] bg-gris"></span>
                     </div>
                     <div class=' font-body text-[16px] mt-4 mb-4'><input class='self-start mr-1' type="checkbox"
-                            name="MentionLegal" value="" required><label for="MentionLegal">J'accepte que mes informations soient utilisées pour une prise de
+                            name="MentionLegal" value="" required><label for="MentionLegal">J'accepte que mes
+                            informations soient utilisées pour une prise de
                             contact avec vous.</label></div>
                     <input type="hidden" name="action" value="process_contact_form">
                     <?php wp_nonce_field('process_contact_form', 'contact_form_nonce'); ?>
                     <div class=" flex justify-center">
-                        <input
-                            class=" hover:!bg-[#FFACAC]  !text-blanc hover:!text-gris mb-7"
-                            type="submit" value="ENVOYER">
+                        <input class=" hover:!bg-[#FFACAC]  !text-blanc hover:!text-gris mb-7" type="submit"
+                            value="ENVOYER">
                     </div>
             </form>
         </div>
