ca5746c8c072d66999de5274ec525ff7af6ac842
steak-osaure <marion.mura090@gmail.com>
2024-03-08 22:15:58 +0100

    structure -agencement

--- wp-content/themes/steaktheme/single-mmi.php
+++ wp-content/themes/steaktheme/single-mmi.php
@@ -12,7 +12,7 @@
             </h3>
             <div class="">
                 <?php $term_image = get_field('photo'); ?>
-                <?php the_post_thumbnail( 'thumbnail' ); ?>
+                <?php the_post_thumbnail('thumbnail'); ?>
                 <!-- image mise en avant servant de logo -->
             </div>
 
@@ -24,47 +24,68 @@
                     </h2>
 
 
-                    <div >
+                    <div class="grid grid-cols-2">
                         <?php the_field('p_resume'); ?>
-                        <div class="grid grid-cols-2">
-                        <?php $image_id = get_field('logo_du_projet'); ?>
+
+                        <div>
+                            <?php $image_id = get_field('logo_du_projet'); ?>
                             <!-- // Récupérer l'ID de l'image -->
 
-                        <img class="" src="<?php echo wp_get_attachment_url($image_id); ?>" alt="img" />
+                            <img class="img-article" src="<?php echo wp_get_attachment_url($image_id); ?>" alt="img" />
                         </div>
-                        
+                    </div>
 
-                        <div>
+
+                    <div>
+                        <h2>
                             <?php the_field('h2-outils_utilises'); ?>
-                        </div>
+                        </h2>
+                    </div>
 
-                        <div>
+                    <div>
+                        <h2>
                             <?php the_field('h2-etapes_de_creation'); ?>
+                        </h2>
+                        <h3>
                             <?php the_field('h3-croquis'); ?>
+                        </h3>
 
+                        <div class="grid grid-cols-2 gap-2">
                             <div>
                                 <?php the_field('p-h3'); ?>
 
-                                <?php $image2_id = get_field('img1-h3'); ?>
-                                <img class="" src="<?php echo wp_get_attachment_url($image2_id); ?>" alt="img" />
-                                <?php the_field('p2-h3'); ?>
-
-                                <?php $image3_id = get_field('img2-h3'); ?>
-                                <img class="" src="<?php echo wp_get_attachment_url($image3_id); ?>" alt="img" />
+                                <div class="w-[12rem]">
+                                    <?php $image2_id = get_field('img1-h3'); ?>
+                                    <img class="w-[12rem]" src="<?php echo wp_get_attachment_url($image2_id); ?>" alt="img" />
+                                </div>
                             </div>
-
                             <div>
-                                <?php the_field('h3-creation'); ?>
-                                <?php the_field('p-h3creation'); ?>
-
-                                <?php $image4_id = get_field('img-h3creation'); ?>
-                                <img class="" src="<?php echo wp_get_attachment_url($image4_id); ?>" alt="img" />
-
+                                <?php the_field('p2-h3'); ?>
                                 <div>
-                                    <a href="<?php the_field('behance'); ?>">Behance</a>
-                                    <a href="<?php the_field('instagram'); ?>">Instagram</a>
-                                    <a href="<?php the_field('youtube'); ?>">Youtube</a>
+                                    <?php $image3_id = get_field('img2-h3'); ?>
+                                    <img class="" src="<?php echo wp_get_attachment_url($image3_id); ?>" alt="img" />
+                                </div>
+                            </div>
+                        </div>
 
+                        <div>
+                            <h3>
+                                <?php the_field('h3-creation'); ?>
+                            </h3>
+                            <div class="grid grid-cols-2 gap-2">
+                                <div>
+                                    <?php the_field('p-h3creation'); ?>
+                                    <div>
+                                        <?php $image4_id = get_field('img-h3creation'); ?>
+                                        <img class="" src="<?php echo wp_get_attachment_url($image4_id); ?>" alt="img" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="grid grid-cols-3">
+                                        <a href="<?php the_field('behance'); ?>">Behance</a>
+                                        <a href="<?php the_field('instagram'); ?>">Instagram</a>
+                                        <a href="<?php the_field('youtube'); ?>">Youtube</a>
+                                    </div>
                                 </div>
                             </div>
 
@@ -72,7 +93,6 @@
                     </div>
 
                 </div>
-            </div>
 
         </article>
     <?php endwhile; ?>
