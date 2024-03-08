de0a7acbbde6f7faea51a46f4b76990de8a55217
steak-osaure <marion.mura090@gmail.com>
2024-03-08 20:55:41 +0100

    recup des données wp ACF CPTUI

--- wp-content/themes/steaktheme/single-mmi.php
+++ wp-content/themes/steaktheme/single-mmi.php
@@ -1,54 +1,69 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : ?>
+<?php if (have_posts()): ?>
 
-   <?php while (have_posts()) : the_post(); ?>
-       <article class="">
-           <!-- <h1 class="title">
-           <?php the_title(); ?> -->
-           </h1>
-           <div class="grid-2-article">
-                          
-               <div class="grid-2-row-article">   
-                
-               <div class="">
-                <?php 
-                    $term_image = get_field('photo'); ?>
-                    <img class="img-article" src="<?php echo esc_url($term_image); ?>" alt="image">
-                </div>    
+    <?php while (have_posts()):
+        the_post(); ?>
+        <article class="">
+            <h1 class="title">
+           <?php the_title(); ?>
+            </h1>
+            <div class="">
 
-                    <ul>
-                        <div class="nom-prenom">
-                            <li > <?php the_field('date_du_projet');?></li>
+                <div class="">
+
+                    <div class="">
+                        <?php
+                        $term_image = get_field('photo'); ?>
+                        <img class="img-article" src="<?php echo esc_url($term_image); ?>" alt="image">
+                    </div>
+
+                    <div class="">
+                        <?php the_field('date_du_projet'); ?>
+                        <?php the_field('h1_article'); ?>
+                    </div>
+
+                    <div class="">
+                        <?php the_field('p_resume'); ?>
+                        <div>
+                            <?php the_field('logo_du_projet'); ?>
+                            <img class="img-article" src="<?php echo esc_url($term_image); ?>" alt="image">
                         </div>
 
-                        <li class="li-info"> <?php the_field('h1_article');?></li>
-                        
-                        <li class="li-info">résumé <?php the_field('p_resume');?></li>
-                         
-                    </ul>
+                        <div>
+                            <?php the_field('h2-outils_utilises'); ?>
+                        </div>
 
-                    <!-- <?php 
-                    $taxonomy = 'postes';
-                    $terms = get_terms( $taxonomy, array(
-                        'exclude_tree' => '10,3', // pour exclure des taxonomies
-                        'orderby' => 'slug'
-                    ) );
-                    foreach ( $terms as $term ) {
-                        echo $term->name;}
-                 the_terms( $post->ID, 'postes', ' ' ); 
-                ?> -->
-               
+                        <div>
+                            <?php the_field('h2-etapes_de_creation'); ?>
+                            <?php the_field('h3-croquis'); ?>
+
+                            <div>
+                                <?php the_field('p-h3'); ?>
+                                <img class="img1-h3" src="<?php echo esc_url($term_image); ?>" alt="image">
+                                <?php the_field('p2-h3'); ?>
+                                <img class="img2-h3" src="<?php echo esc_url($term_image); ?>" alt="image">
+                            </div>
+
+                            <div>
+                                <?php the_field('h3-creation'); ?>
+                                <?php the_field('p-h3creation'); ?>
+                                <img class="img-h3creation" src="<?php echo esc_url($term_image); ?>" alt="image">
+
+                                <div>
+                                    <?php the_field('Behance'); ?>
+                                    <?php the_field('Instagram'); ?>
+                                    <?php the_field('Youtube'); ?>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
-                <div>
-                    <?php the_content(); ?>
-                </div>
-           </div>
-           
-       </article>
-   <?php endwhile; ?>
+            </div>
+
+        </article>
+    <?php endwhile; ?>
 <?php endif; ?>
 <?php get_footer(); ?>
-
-
-
-<!-- <?php the_terms( $post->ID, 'equipe', 'equipe : ' ); ?><br> -->
