5b568f0b663a30225db098a0df8ffb3e0e0c2fb9
steak-osaure <marion.mura090@gmail.com>
2024-03-21 21:45:21 +0100

    archive-perso fait

---
+++ wp-content/themes/steaktheme/archive-perso.php
@@ -0,0 +1,101 @@
+<?php get_header(); ?>
+
+<div class="grid grid-cols-1 justify-items-center md:grid-cols-1 items-center gap-0 pt-10 pb-[10rem] md:pb-[5rem] sm:mx-[2rem] md:mx-[2rem] mx-[12vw]">
+    <div class="">
+        <img class="absolute z-[-1] w-[480px] left-[-100px] top-[50px] md:top-[0px] sm:w-[400px] sm:top-[-110px] sm:left-[-40px] " src="<?php echo wp_get_attachment_url(16); ?>" alt="cercle violet blur" />
+        <img class="absolute z-[-1] w-[250px] top-[320px] left-[200px] md:w-[200px] md:left-[150px] md:top-[380px] sm:w-[200px] sm:left-10 sm:top-[200px]" src="<?php echo wp_get_attachment_url(18); ?>" alt="cercle vert blur" />
+        <img class="absolute z-[-1] w-[240px] top-0 left-[180px] md:w-[200px] md:left-[200px] sm:w-[190px] sm:left-[190px] sm:top-[-70px] overflow-hidden" src="<?php echo wp_get_attachment_url(17); ?>" alt="cercle jaune blur" />
+    </div>
+    <div class="z-40 flex flex-row-reverse md:grid md:grid-cols-1 gap-[5rem] justify-items-center items-center pt-[50px] md:py-[2rem]">
+        <a href="https://www.instagram.com/steakosaure.png" target="_blank">
+            <img class="md:w-[185px] shadowbulle" src="<?php echo wp_get_attachment_url(91); ?>" alt="icone behance" /></a>
+        <div>
+            <p class="text-gris text-[2rem] font-Paytone md:text-[22px] sm:text-[1rem] z-40">Pendant mon temps perso je dessine c'est cadeau</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="grid grid-cols-3 md:grid-cols-1 sm:grid-cols-1  gap-0 m-0 p-0">
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) :
+                    the_post(); ?>
+            // à ajouter dans article et div plus haut border border-gray-300 pour voir la grid 
+            <article class=" ">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('large'); ?> </a>
+                <div class="">
+                    <h3 class="title">
+                        
+                            <?php the_title(); ?>
+                    </h3>
+                    <p class="description">voir le projet</p>
+                    
+
+                </div> 
+            </article> // boucle pour récup tous les articles
+            // on va mettre en triant par type d'articles visuellement - tout design puis tout animation puis tout ce qui reste s'il y a d'autre cat
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+
+
+    <!-- afficher les articles sur l'animation -->
+    <!-- <?php echo '<br/>';
+
+    $cat_taxonomie = 'projetperso';
+    $args = array(
+        'showposts' => '6',
+        'tax_query' => array(
+            array(
+                'taxonomy' => $cat_taxonomie,
+                'field'    => 'slug',
+                'terms'    => array('')
+
+            )
+        ),
+    );
+    $the_query = new WP_query($args);
+
+    if ($the_query->have_posts()) {
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
+            echo '<a href="' . get_permalink() . '">';
+            the_post_thumbnail('large');
+            echo '</a>';
+        }
+    }
+    wp_reset_postdata();
+
+    // afficher les articles sur le design
+    $args2 = array(
+        'showposts' => '6',
+        'tax_query' => array(
+            array(
+                'taxonomy' => $cat_taxonomie,
+                'field'    => 'slug',
+                'terms'    => array('design')
+
+            )
+        ),
+    );
+
+    $the_query2 = new WP_query($args2);
+
+    if ($the_query2->have_posts()) {
+        while ($the_query2->have_posts()) {
+            $the_query2->the_post();
+            echo '<a href="' . get_permalink() . '">';
+            the_post_thumbnail('large');
+            echo '</a>';
+        }
+    }
+    wp_reset_postdata();
+    ?> -->
+
+</div>
+
+
+<?php
+
+get_footer(); ?>
---
+++ wp-content/themes/steaktheme/single-perso.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+<?php if (have_posts()): ?>
+
+    <?php while (have_posts()):
+        the_post(); ?>
+        <article class="text-gris sm:mx-[2rem] md:mx-[2rem] mx-[12vw]">
+            <div class="grid grid-cols-1 justify-items-center md:grid-cols-1 items-center gap-0 py-10">
+                <div class="">
+                    <img class="absolute z-[-1] w-[480px] left-[-100px] top-[50px] md:top-[0px] sm:w-[400px] sm:top-[-110px] sm:left-[-40px] "
+                        src="<?php
+                        echo wp_get_attachment_url(16); ?>" alt="cercle violet blur" />
+                    <img class="absolute z-[-1] w-[250px] top-[320px] left-[200px] md:w-[200px] md:left-[150px] md:top-[380px] sm:w-[200px] sm:left-10 sm:top-[200px]"
+                        src="<?php
+                        echo wp_get_attachment_url(18); ?>" alt="cercle orange blur" />
+                    <img class="absolute z-[-1] w-[240px] top-0 left-[180px] md:w-[200px] md:left-[200px] sm:w-[190px] sm:left-[190px] sm:top-[-70px] overflow-hidden"
+                        src="<?php
+                        echo wp_get_attachment_url(17); ?>" alt="cercle vert blur" />
+                </div>
+
+                <div
+                    class="z-40 grid grid-cols-2 md:grid-cols-1 gap-[2rem] justify-items-center items-center pt-[50px] md:py-[2rem]">
+                    <div>
+                        <h1 class=" text-[4rem]  md:text-2xl sm:text-[2rem] pb-5">
+                            <?php the_title(); ?>
+                        </h1>
+                        <h3>
+                            <?php the_field('date_du_projet'); ?>
+                        </h3>
+                    </div>
+                    <div class="">
+                        <?php $term_image = get_field('photo'); ?>
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                        <!-- image mise en avant servant de logo -->
+                    </div>
+                </div>
+            </div>
+
+            <!-- //résumé article -->
+            <h2 class="z-40 text-[2rem] pb-5 md:text-2xl sm:text-[2rem]">
+                <?php the_field('h1_article'); ?>
+            </h2>
+
+            <div class="z-40 mb-[2rem] grid grid-cols-1 justify-items-center content-between ">
+
+                <div class="grid grid-cols-2 md:grid-cols-1 gap-[2rem] sm:gap-[4rem]  mb-[4rem] items-start">
+
+                    <div class="">
+                        <?php the_field('p_resume'); ?>
+                    </div>
+                    <div class="">
+                        <?php $image_id = get_field('logo_du_projet'); ?>
+                        <!-- // Récupérer l'ID de l'image -->
+
+                        <img class="w-[25rem]" src="<?php echo wp_get_attachment_url($image_id); ?>" alt="img" />
+
+                    </div>
+                </div>
+            </div>
+    </article>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
