f25d7e04e82d1a1177ca3700ec3acd1df7d310d1
steak-osaure <marion.mura090@gmail.com>
2024-03-23 13:34:16 +0100

    style archive mmi cool et refonte nikel + responsive

--- wp-content/themes/steaktheme/archive-mmi.php
+++ wp-content/themes/steaktheme/archive-mmi.php
@@ -10,8 +10,7 @@
         <a href="https://www.behance.net/kiwwii_kawaii" target="_blank">
             <img class="md:w-[185px] shadowbulle" src="<?php echo wp_get_attachment_url(93); ?>" alt="icone behance" /></a>
         <div>
-            <p class="text-gris text-[2rem] font-Paytone md:text-[22px] sm:text-[1rem] z-40">Aspirante graphiste voici
-                mes meilleurs travaux réalisés lors de mon BUT MMI</p>
+            <p class="text-gris text-[2rem] font-Paytone md:text-[22px] sm:text-[1rem] z-40">Aspirante graphiste voici mes meilleurs travaux réalisés en BUT MMI</p>
         </div>
     </div>
 </div>
@@ -19,72 +18,147 @@
 <!-- ancienne classe grille de 3 responsive class="grid grid-cols-3 md:grid-cols-1 sm:grid-cols-1  gap-0 m-0 p-0" -->
 <div class="">
 
-    <div class="grid justify-items-start mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem] py-4">
+    <div class="grid justify-items-start mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem] py-[4rem]">
         <h1 class="text-[5rem] text-transparent strokehomepage-motiondesign sm:text-[40px] md:text-[3rem]">Motion design</h1>
-        
+
     </div>
 
-<div class="grid grid-cols-2 gap-2 mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem]">
-    <!-- afficher les articles sur l'animation -->
-    <?php ;
+    <div class="grid grid-cols-2 md:grid-cols-1 gap-2 mx-[8rem] lg:mx-[6rem] md:mx-[2rem] sm:mx[1rem] pb-[4rem]">
+        <!-- afficher les articles sur l'animation -->
+        <?php
 
-    $cat_taxonomie = 'articlemmi';
-    $args = array(
-        'showposts' => '6',
-        'tax_query' => array(
-            array(
-                'taxonomy' => $cat_taxonomie,
-                'field'    => 'slug',
-                'terms'    => array('animation')
+        $cat_taxonomie = 'articlemmi';
+        $args = array(
+            'showposts' => '6',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => $cat_taxonomie,
+                    'field'    => 'slug',
+                    'terms'    => array('animation')
 
-            )
-        ),
-    );
-    $the_query = new WP_query($args);
+                )
+            ),
+        );
+        $the_query = new WP_query($args);
 
-    if ($the_query->have_posts()) {
-        while ($the_query->have_posts()) {
-            $the_query->the_post();
-            echo '<a href="' . get_permalink() . '"><div class="grid grid-cols-2 gap-2">';
-            the_post_thumbnail('medium');
-            the_title();
-            
-            echo '</div></a>';
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                echo '<a href="' . get_permalink() . '">
+            <div class="grid grid-cols-2 sm:grid-cols-1 gap-2">';
+                the_post_thumbnail('large');
+                echo '<div class=""><h3>';
+                the_title();
+
+
+                echo '</h3>
+            <span class="block justify-items-center w-[85%] lg:w-[100%] h-[2px]  bg-gris"></span>
+            <p>';
+                the_field('date_du_projet');
+                echo '</p><p>';
+                the_field('petit_resume');
+
+                echo '</p></div>
+            </div>
+            </a>';
+            }
         }
-    }
-    wp_reset_postdata(); ?>
-</div>
-    <div class="grid justify-items-start mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem]">
+        wp_reset_postdata(); ?>
+    </div>
+    
+    <div class="grid justify-items-start mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem] py-[4rem]">
         <h1 class="text-[5rem] grid text-transparent strokehomepage-webdesign sm:text-[40px] md:text-[3rem]">Web design</h1>
-        <span class="block justify-items-center w-[85%] lg:w-full  h-[2px] mt-[-20px] md:mt-[-5px] bg-gris"></span>
     </div>
 
-    <?php // afficher les articles sur le design
-    $args2 = array(
-        'showposts' => '6',
-        'tax_query' => array(
-            array(
-                'taxonomy' => $cat_taxonomie,
-                'field'    => 'slug',
-                'terms'    => array('design')
+    <div class="grid grid-cols-2 md:grid-cols-1 gap-2 mx-[8rem] lg:mx-[6rem] md:mx-[2rem] sm:mx[1rem] pb-[4rem]">
+        <!-- afficher les articles sur en design -->
+        <?php
 
-            )
-        ),
-    );
+        $cat_taxonomie = 'articlemmi';
+        $args = array(
+            'showposts' => '6',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => $cat_taxonomie,
+                    'field'    => 'slug',
+                    'terms'    => array('design')
 
-    $the_query2 = new WP_query($args2);
+                )
+            ),
+        );
+        $the_query = new WP_query($args);
 
-    if ($the_query2->have_posts()) {
-        while ($the_query2->have_posts()) {
-            $the_query2->the_post();
-            echo '<a href="' . get_permalink() . '">';
-            the_post_thumbnail('large');
-            echo '</a>';
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                echo '<a href="' . get_permalink() . '">
+            <div class="grid grid-cols-2 sm:grid-cols-1 gap-2">';
+                the_post_thumbnail('large');
+                echo '<div class=""><h3>';
+                the_title();
+
+
+                echo '</h3>
+            <span class="block justify-items-center w-[85%] lg:w-[100%] h-[2px]  bg-gris"></span>
+            <p>';
+                the_field('date_du_projet');
+                echo '</p><p>';
+                the_field('petit_resume');
+
+                echo '</p></div>
+            </div>
+            </a>';
+            }
         }
-    }
-    wp_reset_postdata();
-    ?>
+        wp_reset_postdata(); ?>
+    </div>
 
+    <div class="grid justify-items-start mx-[12rem] lg:mx-[8rem] md:mx-[2rem] sm:mx[1rem] py-[4rem]">
+        <h1 class="text-[5rem] grid text-transparent strokehomepage-graphisme sm:text-[40px] md:text-[3rem]">Graphisme</h1>
+    </div>
+
+    <div class="grid grid-cols-2 md:grid-cols-1 gap-2 mx-[8rem] lg:mx-[6rem] md:mx-[2rem] sm:mx[1rem] pb-[4rem]">
+        <!-- afficher les articles en graphisme -->
+        <?php
+
+        $cat_taxonomie = 'articlemmi';
+        $args = array(
+            'showposts' => '6',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => $cat_taxonomie,
+                    'field'    => 'slug',
+                    'terms'    => array('graphisme')
+
+                )
+            ),
+        );
+        $the_query = new WP_query($args);
+
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                echo '<a href="' . get_permalink() . '">
+            <div class="grid grid-cols-2 sm:grid-cols-1 gap-2">';
+                the_post_thumbnail('large');
+                echo '<div class=""><h3>';
+                the_title();
+
+
+                echo '</h3>
+            <span class="block justify-items-center w-[85%] lg:w-[100%] h-[2px]  bg-gris"></span>
+            <p>';
+                the_field('date_du_projet');
+                echo '</p><p>';
+                the_field('petit_resume');
+
+                echo '</p></div>
+            </div>
+            </a>';
+            }
+        }
+        wp_reset_postdata(); ?>
+    </div>
 
 
     <!-- <?php if (have_posts()) : ?>
