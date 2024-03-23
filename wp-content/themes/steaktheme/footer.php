f6f17f1edf56216e526486ee7a819aa5a4a3cb33
steak-osaure <marion.mura090@gmail.com>
2024-03-23 20:43:34 +0100

    corrections responsive et article single perso

--- wp-content/themes/steaktheme/footer.php
+++ wp-content/themes/steaktheme/footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <div class="grid grid-cols-2 md:grid-cols-1 justify-items-center items-center ">
+  <div class="grid grid-cols-2 md:grid-cols-1 justify-items-center items-center md:pb-[2rem]">
     <div class="grid grid-cols-2 md:grid-cols-1 items-center">
     <img class="w-[80px] m-5" src="<?php echo get_template_directory_uri(); ?>/favicon.svg" alt="logo" />
     <h1 class="text-blanc md:pb-8">Contactez moi !</h1>
@@ -8,7 +8,7 @@
       <?php wp_nav_menu(array(  'theme_location' => 'footer-menu'  )); ?></nav> -->
 
 
-    <div class="grid grid-cols-3 gap-[2rem] justify-center items-center justify-items-center">
+    <div class="grid grid-cols-3 gap-[2rem] justify-center items-center justify-items-center ">
       <a href="https://www.instagram.com/steakosaure.png/" target="_blank">
         <img class="h-[30px]" src="<?php echo wp_get_attachment_url(130); ?>" alt="icone instagram" /></a>
 
